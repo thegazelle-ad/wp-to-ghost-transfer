678769fd6f544bbb6b81b3756fa1b56446bf25e8
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2017-10-16 15:48:17 -0400

    Add update teams script for fall 2017 (extra modularization added)

---
+++ update-teams-fall-2017.js
@@ -0,0 +1,104 @@
+'use strict';
+// Zubareva, Paula Dozsa, Ji Young Kim, Ikenna, Cynthia
+
+const management = ['tom-klein', 'kristina-stankovic', 'karolina-wilczynska', 'hannah-taylor', 'annie-bauer', 'nico-raney', 'ayah-rashid', 'karma-gurung', 'bernice-delos-reyes', 'julia-tymoshenko'];
+const editorial = ['shreya-shreeraman', 'priyanka-lakhiani', 'vamika-sinha', 'nikolaj-nielsen', 'jakob-plaschke', 'paula-estrada', 'jocilyn-estes', 'daria-zahaleanu', 'argentina-mena', 'andrea-arletti', 'dania-paul', 'herbert-crowther', 'thirangie-jayatilake', 'nimrah-khanyari', 'warda-malik', 'natalie-kopczewski', 'nathan-quimpo', 'diya-gupta', 'cece-kim', 'chaerin-lim'];
+const multimedia = ['melinda-szekeres', 'joaquin-kunkel', 'katarina-holtzapple', 'ilona-szekeres', 'shenuka-corea', 'mahgul-farooqui', 'miha-klasinc', 'seongun-si', 'simran-parwani', 'alexis-mountcastle', 'aizaz-arif-ansari'];
+const writers = ['taj-chapman', 'paula-valentina', 'aron-braunstiner', 'daniah-kheetan', 'gaurav-dewani', 'simrat-roopra', 'archita-arun', 'dana-abu-ali', 'abdelrahman', 'hind-ait-mout', 'nandini-kochar', 'soohyun-hwangbo'];
+const web = ['emil-goldsmith-olesen', 'zane-mountcastle', 'jaisal-friedman', 'arantza-rodriguez'];
+
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    "host": "127.0.0.1",
+    "user": "root",
+    "password": "password",
+    "database": "the_gazelle",
+    "charset": "latin1"
+  },
+});
+
+knex('teams').insert([
+  {
+    'name': 'multimedia',
+    'slug': 'multimedia',
+  },
+  {
+    'name': 'writers',
+    'slug': 'writers',
+  },
+  {
+    'name': 'web',
+    'slug': 'web',
+  },
+])
+.then(() => {
+  console.log("Teams inserted successfully");
+  return knex('semesters').insert([
+    {
+      'name': 'Fall 2017',
+      'date': '2017-09-01',
+    },
+  ]);
+})
+.then(() => {
+  console.log("Semesters inserted successfully");
+  populateTeam(management, 'management', 'Fall 2017', 0);
+  populateTeam(editorial, 'editorial', 'Fall 2017', 1);
+  populateTeam(multimedia, 'multimedia', 'Fall 2017', 2);
+  populateTeam(writers, 'writers', 'Fall 2017', 3);
+  populateTeam(web, 'web', 'Fall 2017', 4);
+});
+
+
+function populateTeam(slugs, teamSlug, semesterName, teamOrder) {
+  let semesterId = null;
+  let teamId = null;
+  knex.select('id').from('semesters').where('name', semesterName)
+  .then(semesterRows => {
+    if (semesterRows.length != 1) {
+      throw new Error("Can't find the semester");
+    }
+    return semesterRows[0].id;
+  })
+  .then(fetchedSemesterId => {
+    semesterId = fetchedSemesterId;
+    return knex.select('id').from('teams').where('slug', teamSlug);
+  })
+  .then(teamRows => {
+    if (teamRows.length != 1) {
+      throw new Error("Can't find the team");
+    }
+    return teamRows[0].id;
+  })
+  .then(fetchedTeamId => {
+    teamId = fetchedTeamId;
+
+    // Time to actually insert
+    const insert = [];
+    const num = slugs.length;
+    let cur = 0;
+    slugs.forEach((slug, index) => {
+      knex.select('id').from('authors').where('slug', slug).then((rows) => {
+        if (rows.length != 1) {
+          console.log(`row length equals ${rows.length} at ${teamSlug} slug: ${slug}`);
+        }
+        insert.push({
+          team_id: teamId,
+          author_id: rows[0].id,
+          team_order: teamOrder,
+          author_order: index,
+          semester_id: semesterId,
+        });
+        cur++;
+        if (cur >= num) {
+          knex('teams_authors').insert(insert).then(() => console.log(`Inserted ${teamSlug}`));
+        }
+      });
+    });
+  })
+  .catch(e => {
+    console.log("ERROR:", e);
+    process.exit(1);
+  });
+}
