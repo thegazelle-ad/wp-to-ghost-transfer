e6023ef5ca92f6064be56f632fdddef116a5dc1c
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-04-15 14:47:27 +0400

    Some modularizing

--- interactiveScripts/updateInteractiveArticle.js
+++ interactiveScripts/updateInteractiveArticle.js
@@ -4,12 +4,13 @@ function disconnect() {
 	knex.destroy();
 }
 
+const password = process.argv[5];
 const knex = require('knex')({
   client: 'mysql',
   connection: {
     host: "localhost",
     user: "root",
-    password: "password",
+    password: password,
     database: "the_gazelle",
     charset: 'latin1'
   }
@@ -18,7 +19,9 @@ const knex = require('knex')({
 const fs = require('fs');
 const articleSlug = process.argv[2];
 const htmlFile = process.argv[3];
+const jsFile = process.argv[4];
 const html = fs.readFileSync(htmlFile, 'utf8');
+const js = fs.readFileSync(jsFile, 'utf8');
 knex.select('id').from('posts').where('slug', '=', articleSlug)
 .then((rows) => {
   if (rows.length < 1) {
@@ -26,7 +29,7 @@ knex.select('id').from('posts').where('slug', '=', articleSlug)
     return;
   }
   const articleId = rows[0].id;
-  return knex('interactive_meta').where('id', '=', articleId).update({ html }).then((numUpdated) => {
+  return knex('interactive_meta').where('id', '=', articleId).update({ html, js }).then((numUpdated) => {
     if (numUpdated === 0) {
       console.log("No record was found for that slug, you should probably use insertInteractiveArticle.js");
     }
